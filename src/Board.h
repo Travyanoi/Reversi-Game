7eb955cda10a7fa6e24a76659e3ea5fa34494810
Ivan Nikiforov <i.nikiforov@stud.sibguti.ru>
2022-12-19 14:09:17 +0700

    Fix start position of figures

--- src/Board.h
+++ src/Board.h
@@ -7,10 +7,10 @@ private:
 public:
     Board(){
         // Заполняем начальное положение первых двух фигур по правилам
-        board[3][3] = 2;
-        board[3][4] = 1;
-        board[4][3] = 1;
-        board[4][4] = 2;
+        board[3][3] = 1;
+        board[3][4] = 2;
+        board[4][3] = 2;
+        board[4][4] = 1;
     }
     ~Board() = default;
 
